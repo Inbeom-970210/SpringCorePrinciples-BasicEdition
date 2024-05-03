7e70029f0419ccde5cf26f39f0d3d71414eea991
Inbeom Kim <dlsqja970210@naver.com>
2024-05-03 20:49:30 +0900

    study: 빈 생명주기 콜백, 빈 스코프

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,40 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        // 파라미터가 컴포넌트 클래스이기 때문에 파라미터가 자동으로 컴포넌트 스캔이 돼서 등록됨
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        // 스프링 컨테이너가 종료되면서 싱글톤 빈도 종료됨
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
